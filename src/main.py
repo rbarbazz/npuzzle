58e52fc1981f96282cec11c74c2dc3aeb8fefd2d
Valentin Paris <codedude@users.noreply.github.com>
2019-05-07 13:46:10 +0200

    ca marche !

--- src/main.py
+++ src/main.py
@@ -5,14 +5,41 @@
 import sys, os
 import copy
 import math
-from collections import deque
+
+# Exemple avec 4x4 => trop long
+# GOAL = "goal_4"
+# BASE = "ex4"
+
+#Exemple 23sec
+# GOAL = "goal_esca"
+# BASE = "hard"
+
+# Exemple 0.15sec
+GOAL = "goal_lin"
+BASE = "exin"
 
 TAQUINS = {
-	"size" : 3,
-	"goal" : [
+	"goal_4" : [
+		1, 2, 3, 4,
+		5, 6, 7, 8,
+		9, 10, 11, 12,
+		13, 14, 15, 0
+	],
+	"ex4" : [
+		12, 5, 8, 10,
+		1, 15, 3, 11,
+		0, 14, 2, 6,
+		4, 7, 9, 13
+	],
+	"goal_esca" : [
 		1, 2, 3,
 		8, 0, 4,
 		7, 6, 5
+	],
+	"goal_lin" : [
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 0
 	],
 	"simple" : [
 		1, 2, 0,
@@ -29,10 +56,16 @@ TAQUINS = {
 		8, 0, 2,
 		7, 4, 5
 	],
+	#21 coups, 386 noeuds
+	"exin" : [
+		4, 8, 3,
+		2, 0, 7,
+		6, 5, 1
+	],
 	"unsolvable" : [
-		5, 3, 6,
-		4, 0, 8,
-		2, 7, 1
+		1, 2, 3,
+		4, 5, 6,
+		8, 7, 0
 	],
 }
 
@@ -48,20 +81,57 @@ ACTION = {
 	board = list des numeros
 """
 class Taquin:
-	def __init__(self, values):
+	def __init__(self, values, size):
 		self.board = values.copy()
-		self.size = int(math.sqrt(len(values)))
-		self.pos = self.board.index(0)
+		self.size = size
+		self._pos, self._x, self._y = 0, 0, 0
+		self.pos = values.index(0)
 
 	@property
+	def pos(self):
+		return self._pos
+	@property
 	def x(self):
-		return self.pos % self.size
+		return self._x
 	@property
 	def y(self):
-		return self.pos // self.size
+		return self._y
+
+	@pos.setter
+	def pos(self, value):
+		self._pos = value
+		self._x = value % self.size
+		self._y = value // self.size
+
+	def inc_x(self):
+		self.pos = self._pos + 1
+	def dec_x(self):
+		self.pos = self._pos - 1
+	def inc_y(self):
+		self.pos = self._pos + self.size
+	def dec_y(self):
+		self.pos = self._pos - self.size
 
 	def __eq__(self, other):
 		return self.board == other.board
+
+	def move(self, action):
+		if action == ACTION["N"]:
+			self.board[self.pos] = self.board[self.pos - self.size]
+			self.dec_y()
+			self.board[self.pos] = 0
+		elif action == ACTION["S"]:
+			self.board[self.pos] = self.board[self.pos + self.size]
+			self.inc_y()
+			self.board[self.pos] = 0
+		elif action == ACTION["E"]:
+			self.board[self.pos] = self.board[self.pos + 1]
+			self.inc_x()
+			self.board[self.pos] = 0
+		elif action == ACTION["W"]:
+			self.board[self.pos] = self.board[self.pos - 1]
+			self.dec_x()
+			self.board[self.pos] = 0
 
 	def __str__(self):
 		r = ""
@@ -71,76 +141,55 @@ class Taquin:
 			r += "{}, ".format(v)
 		return r
 
-	def __copy__(self):
-		cpy = type(self)(self.board)
-		return cpy
-
-	def move(self, action):
-		if action == ACTION["N"]:
-			self.board[self.pos] = self.board[self.pos - self.size]
-			self.pos -= self.size
-			self.board[self.pos] = 0
-		elif action == ACTION["S"]:
-			self.board[self.pos] = self.board[self.pos + self.size]
-			self.pos += self.size
-			self.board[self.pos] = 0
-		elif action == ACTION["E"]:
-			self.board[self.pos] = self.board[self.pos + 1]
-			self.pos += 1
-			self.board[self.pos] = 0
-		elif action == ACTION["W"]:
-			self.board[self.pos] = self.board[self.pos - 1]
-			self.pos -= 1
-			self.board[self.pos] = 0
-
-def calc_heur(taquin):
+def heuristic_manhattan(goal, current):
 	tt = 0
-	for i, v in enumerate(taquin.board):
-		i_goal = TAQUINS["goal"].index(v)
-		gx, gy = i_goal % 3, i_goal // 3
-		tt += abs(taquin.x - gx) + abs(taquin.y - gy)
+	size = goal.size
+	for i, v in enumerate(current.board):
+		if v == 0:
+			continue
+		i_goal = goal.board.index(v)
+		gx, gy = i_goal % size, i_goal // size
+		tx, ty = i % size, i // size
+		tt += abs(tx - gx) + abs(ty - gy)
 	return tt
+
+taquin_goal = Taquin(TAQUINS[GOAL], int(math.sqrt(len(TAQUINS[GOAL]))))
 
 """
 	Etat en cours
 """
 class State:
-	def __init__(self, taquin):
+	def __init__(self, taquin, up_heur=False):
 		self.__cost = 0			# +1 each step
-		self.__heuristic = calc_heur(taquin)	# 0 means goal
+		self.__heuristic = 0	# 0 means goal
 		self.__action = -1	# ACTION
 		self.__parent = None	# Ref to previous state
-		self.__taquin = copy.copy(taquin)	# Current taquin after transition
-
-	def __copy__(self):
-		cpy = type(self)(self.__taquin)
-		return cpy
+		self.__taquin = copy.deepcopy(taquin) # Current taquin after transition
+		self.__weight = 0
+		if up_heur:
+			self.__heuristice = heuristic_manhattan(
+				taquin_goal, self.__taquin)
+			self.up_weight()
 
 	def transition(self, action):
-		new_state = copy.copy(self)
+		new_state = type(self)(self.__taquin)
+		new_state.__taquin.move(action)
+		new_state.__heuristic = heuristic_manhattan(
+				taquin_goal, new_state.__taquin)
+		new_state.__cost = self.__cost + 1
 		new_state.__action = action
 		new_state.__parent = self
-		# print("Before move {}:".format(action))
-		# print(new_state.__taquin)
-		new_state.__taquin.move(action)
-		# print("After move:")
-		# print(new_state.__taquin)
-		# print()
-		new_state.__heuristic = calc_heur(new_state.__taquin)
-
+		new_state.up_weight()
 		return new_state
 
-	def inc_cost(self):
-		self.__cost += 1
-
 	def __lt__(self, other):
-		return self.__heuristic < other.__heuristic
+		return self.weight < other.weight
 	def __gt__(self, other):
-		return self.__heuristic > other.__heuristic
+		return self.weight > other.weight
 	def __le__(self, other):
-		return self.__heuristic <= other.__heuristic
+		return self.weight <= other.weight
 	def __ge__(self, other):
-		return self.__heuristic >= other.__heuristic
+		return self.weight >= other.weight
 
 	def __eq__(self, other):
 		return self.__taquin == other.__taquin
@@ -161,8 +210,23 @@ class State:
 	def y(self):
 		return self.__taquin.y
 
+	@property
+	def taquin(self):
+		return self.__taquin
+
+	@property
+	def cost(self):
+		return self.__cost
+	@property
+	def heuristic(self):
+		return self.__heuristic
+
+	@property
 	def weight(self):
-		return self.__cost + self.__heuristic
+		return self.__weight
+
+	def up_weight(self):
+		self.__weight = self.__cost + self.__heuristic
 
 	@property
 	def action(self):
@@ -204,52 +268,76 @@ def astar(start, goal):
 	open_lst = PriorityList()
 	close_lst = []
 	counter = 0
-	final_state = State(goal)
-	open_lst.append(State(start))
+	counter_node = 0
+	final_state = State(goal, True)
+	open_lst.append(State(start, True))
 	found = None
 	while (len(open_lst) > 0):
 		curr_state = open_lst.pop()
+		close_lst.append(curr_state)
+
 		if curr_state == final_state:
 			found = curr_state
 			break
 
 		for next_state in find_moves(curr_state):
-			#print(next_state)
-			try:
-				old_o = open_lst.index(next_state)
-			except ValueError:
-				old_o = None
 			try:
 				old_c = close_lst.index(next_state)
 			except ValueError:
 				old_c = None
-			if (old_o is not None and open_lst[old_o].weight() < next_state.weight()) \
-			or (old_c is not None and close_lst[old_c].weight() < next_state.weight()):
-				pass
-			else:
-				next_state.inc_cost()
+			if old_c is not None \
+			and next_state.weight < close_lst[old_c].weight:
+				close_lst.pop(old_c)
 				open_lst.append(next_state)
-			#print()
-		#print("\nNew turn")
+				continue
+
+			try:
+				old_o = open_lst.index(next_state)
+			except ValueError:
+				old_o = None
+			if old_o is not None \
+			and next_state.weight < open_lst[old_o].weight:
+				open_lst.pop(old_o)
+				open_lst.append(next_state)
+				continue
+
+			open_lst.append(next_state)
+			counter_node += 1
+
 		counter += 1
-		if counter % 100 == 0:
+		if counter % 1000 == 0:
 			print(counter)
-		close_lst.append(curr_state)
 
 	if found is not None:
-		print("Success {}".format(counter))
+		print("Success: turn = {}, nodes = {}".format(counter, counter_node))
 	else:
-		print("Failure {}".format(counter))
+		print("Failure: turn = {}, nodes = {}".format(counter, counter_node))
 		return 0
 
+	nb_hit = 0
 	while (found.parent is not None):
-		print(found)
 		found = found.parent
+		nb_hit += 1
+	print("Moves: {}".format(nb_hit))
 
+def solvable(taquin):
+	inv = 0
+	for i, v in enumerate(taquin.board):
+		if v == 0:
+			continue
+		for w in taquin.board[i:]:
+			if w == 0:
+				continue
+			if v > w:
+				inv += 1
+	return inv % 2 == 0
 
 def main():
-	taquin_base = Taquin(TAQUINS["other"])
-	taquin_goal = Taquin(TAQUINS["goal"])
+	taquin_base = Taquin(TAQUINS[BASE],
+		int(math.sqrt(len(TAQUINS[BASE]))))
+	if not solvable(taquin_base):
+		print("Not solvable")
+		#return 0
 
 	astar(taquin_base, taquin_goal)
 
