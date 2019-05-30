c98a61cd68b5a3cd46e7ce48d0c2ddba34f96092
Valentin PARIS <vparis@student.42.fr>
2019-05-30 19:29:43 +0200

    rework argparse, not done

---
+++ npuzzle/api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+#
+
+
+import sys
+
+# import parser
+
+
+__all__ = ["make_random", "make_from_string", "solve"]
+
+
+def make_random(type, size, solvable, iterations):
+	pass
+
+
+def make_from_string(type, string_input):
+	pass
+
+
+def solve(type, string_input, greedy, heuristic):
+	pass
+
+
+if __name__ == '__main__':
+	sys.exit(0)
--- npuzzle/consts.py
+++ npuzzle/consts.py
@@ -3,8 +3,8 @@
 #
 
 SIZE = 0
-HEUR = 1
-GREEDY = True
+HEUR = 2
+GREEDY = False
 SOLVABLE = True
 ITERATIONS = 3000
 CONTINUE = False
