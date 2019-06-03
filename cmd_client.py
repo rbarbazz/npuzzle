755e02c66875862fa776485e54d4fb53b10a6be7
Valentin Paris <codedude@users.noreply.github.com>
2019-06-03 16:45:08 +0200

    up

--- cmd_client.py
+++ cmd_client.py
@@ -3,36 +3,60 @@
 #
 
 import sys
-import signal
+import time
 
-from npuzzle import api
-from npuzzle import parser
+from npuzzle import api, parser, gen, npuzzle
 
 
-def signal_handler(sig, frame):
-	if sig == signal.SIGINT:
-		sys.exit(0)
+def callback(data):
+	print("Thread responds:")
+	print(data)
 
 
 def main():
-	signal.signal(signal.SIGINT, signal_handler)
+	r = gen.gen_random("snale", 3, 3000, True)
+	r1 = npuzzle.make_taquin(r)
+	print("Base:\n{}".format(r1))
+	print(api.check_solvability("snale", r))
+	r = api.solve("snale", r, True, "manhattan", callback)
+	if r["error"]:
+		print(r["data"])
+		return 0
+	# print(r)
+	api.wait_solving()
+	return 0
+
 	parser.sanitize_arguments()
 
-	r = api.make_random("snale", 3, True)
+	print("Heuristics: {}".format(api.get_available_heuristics()))
+	print("Types: {}".format(api.get_available_types()))
+	print("Make random:")
+	r = api.make_random("snale", 4, True, 1000)
 	if r["error"]:
 		print(r["data"])
 		return 0
 	print(r)
+	print("Check solvability:")
 	r = api.check_solvability(r["type"], r["npuzzle"])
 	if r["error"]:
 		print(r["data"])
 		return 0
 	print(r)
-	r = api.solve(r["type"], r["npuzzle"], False, "manhattan")
-	if r["error"]:
-		print(r["data"])
-		return 0
-	print(r)
+	if r["solvable"]:
+		print("Is running ?: {}".format(api.is_solving()))
+		print("Solve:")
+		# Start solve in a new thread
+		r = api.solve(r["type"], r["npuzzle"], False, "manhattan", callback)
+		if r["error"]:
+			print(r["data"])
+			return 0
+		print(r)
+
+	print("\nWait 3secs")
+	api.wait_solving()
+	#Stop the current process and return
+	api.stop_solving()
+
 	return 0
 
 
